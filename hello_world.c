c4fb9c66712da252072b4488e4fe47cf4c8e3007
ykc5056 <ykc5056@psu.edu>
2017-06-08 08:16:10 -0400

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+//# hello_world
+//hello world in c
+
+/*code below:*/
+#include <stdio.h>//head file
+int main()//main function
+{
+  printf("Hello World");//display hello world
+  return 0;//return value
+}
+/*end the code*/
